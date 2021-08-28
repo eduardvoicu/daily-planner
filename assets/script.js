0872b1f28ee3aafc36aad77b74eff2056a5432df
Eduard Voicu <iameduardvoicu@gmail.com>
2021-08-27 23:21:40 -0400

    Created function for hourly color change for pastTime, currentTime and futureTime equation.

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,11 @@ var todaysDate = moment().format("MMM Do, YYYY, hh:mm:ss");
 $("#currentDay").text(todaysDate);
 
 function setTime() {
-    var timerInterval = setInterval(function() {
-        todaysDate = moment().format("MMM Do, YYYY, hh:mm:ss");
-        $("#currentDay").text(todaysDate);
-    }, 1000);
-  }
+  var timerInterval = setInterval(function () {
+    todaysDate = moment().format("MMM Do, YYYY, hh:mm:ss");
+    $("#currentDay").text(todaysDate);
+  }, 1000);
+}
 
 setTime();
 
@@ -16,3 +16,25 @@ setTime();
 // It will be a little redundant as each time entry will have its own variable and function.
 // Will update later once a solution is found.
 
+var timeNow = new Date().getHours();
+
+function colorTime() {
+  // 0700 time block function
+  if (timeNow < 7) {
+    $("#colorEntry1").addClass("futureTime");
+  } else if (timeNow == 7) {
+    $("#colorEntry1").addClass("currentTime");
+  } else if (timeNow > 7) {
+    $("#colorEntry1").addClass("pastTime");
+  }
+
+  // 0800 time block funtion
+  if (timeNow < 7) {
+    $("#colorEntry1").addClass("futureTime");
+  } else if (timeNow == 7) {
+    $("#colorEntry1").addClass("currentTime");
+  } else if (timeNow > 7) {
+    $("#colorEntry1").addClass("pastTime");
+  }
+}
+colorTime();
