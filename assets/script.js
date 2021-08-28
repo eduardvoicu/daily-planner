b8fecf008166ece2e890faa31dd14ba141973c44
Eduard Voicu <iameduardvoicu@gmail.com>
2021-08-28 15:51:30 -0400

    Finished function for changing colors on past, current, future time blocks 07-17.

--- assets/script.js
+++ assets/script.js
@@ -29,12 +29,93 @@ function colorTime() {
   }
 
   // 0800 time block funtion
-  if (timeNow < 7) {
-    $("#colorEntry1").addClass("futureTime");
-  } else if (timeNow == 7) {
-    $("#colorEntry1").addClass("currentTime");
-  } else if (timeNow > 7) {
-    $("#colorEntry1").addClass("pastTime");
+  if (timeNow < 8) {
+    $("#colorEntry2").addClass("futureTime");
+  } else if (timeNow == 8) {
+    $("#colorEntry2").addClass("currentTime");
+  } else if (timeNow > 8) {
+    $("#colorEntry2").addClass("pastTime");
+  }
+
+  // 0900 time block function
+  if (timeNow < 9) {
+    $("#colorEntry3").addClass("futureTime");
+  } else if (timeNow == 9) {
+    $("#colorEntry3").addClass("currentTime");
+  } else if (timeNow > 9) {
+    $("#colorEntry3").addClass("pastTime");
+  }
+
+  // 1000 time block function
+  if (timeNow < 10) {
+    $("#colorEntry4").addClass("futureTime");
+  } else if (timeNow == 10) {
+    $("#colorEntry4").addClass("currentTime");
+  } else if (timeNow > 10) {
+    $("#colorEntry4").addClass("pastTime");
+  }
+
+  // 1100 time block function
+  if (timeNow < 11) {
+    $("#colorEntry5").addClass("futureTime");
+  } else if (timeNow == 11) {
+    $("#colorEntry5").addClass("currentTime");
+  } else if (timeNow > 11) {
+    $("#colorEntry5").addClass("pastTime");
+  }
+
+  // 1200 time block function
+  if (timeNow < 12) {
+    $("#colorEntry6").addClass("futureTime");
+  } else if (timeNow == 12) {
+    $("#colorEntry6").addClass("currentTime");
+  } else if (timeNow > 12) {
+    $("#colorEntry6").addClass("pastTime");
+  }
+
+  // 1300 time block function
+  if (timeNow < 13) {
+    $("#colorEntry7").addClass("futureTime");
+  } else if (timeNow == 13) {
+    $("#colorEntry7").addClass("currentTime");
+  } else if (timeNow > 13) {
+    $("#colorEntry7").addClass("pastTime");
+  }
+
+  // 1400 time block function
+  if (timeNow < 14) {
+    $("#colorEntry8").addClass("futureTime");
+  } else if (timeNow == 14) {
+    $("#colorEntry8").addClass("currentTime");
+  } else if (timeNow > 14) {
+    $("#colorEntry8").addClass("pastTime");
+  }
+
+  // 1500 time block function
+  if (timeNow < 15) {
+    $("#colorEntry9").addClass("futureTime");
+  } else if (timeNow == 15) {
+    $("#colorEntry9").addClass("currentTime");
+  } else if (timeNow > 15) {
+    $("#colorEntry9").addClass("pastTime");
+  }
+
+  // 0900 time block function
+  if (timeNow < 16) {
+    $("#colorEntry10").addClass("futureTime");
+  } else if (timeNow == 16) {
+    $("#colorEntry10").addClass("currentTime");
+  } else if (timeNow > 16) {
+    $("#colorEntry10").addClass("pastTime");
+  }
+
+  // 1700 time block function
+  if (timeNow < 17) {
+    $("#colorEntry11").addClass("futureTime");
+  } else if (timeNow == 17) {
+    $("#colorEntry11").addClass("currentTime");
+  } else if (timeNow > 17) {
+    $("#colorEntry11").addClass("pastTime");
   }
 }
 colorTime();
