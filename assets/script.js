f9c1a8ba1f3878191cc3f444346c3fba6cfa8e18
Eduard Voicu <iameduardvoicu@gmail.com>
2021-08-28 16:43:05 -0400

    Creating new variables that get previous entries out of local storage.

--- assets/script.js
+++ assets/script.js
@@ -12,9 +12,9 @@ function setTime() {
 setTime();
 
 // Below are some conditional variables and funtions that will be able
-// to change color based on if the event planner entry.
+// to change color based on if the event planner entry is in the past, current or in the future.
 // It will be a little redundant as each time entry will have its own variable and function.
-// Will update later once a solution is found.
+// Will update later once a solution is found compile multiple hour variables into one.
 
 var timeNow = new Date().getHours();
 
@@ -119,3 +119,128 @@ function colorTimeBlockChange() {
   }
 };
 colorTimeBlockChange()
+
+// Variables for local storage entries
+// Creating variables that are pulling IDs from HTML table body time blocks.
+var timeBlock1 = $("#colorEntry1");
+var timeBlock2 = $("#colorEntry2");
+var timeBlock3 = $("#colorEntry3");
+var timeBlock4 = $("#colorEntry4");
+var timeBlock5 = $("#colorEntry5");
+var timeBlock6 = $("#colorEntry6");
+var timeBlock7 = $("#colorEntry7");
+var timeBlock8 = $("#colorEntry8");
+var timeBlock9 = $("#colorEntry9");
+var timeBlock10 = $("#colorEntry10");
+var timeBlock11 = $("#colorEntry11");
+
+// Function below will start storing entry on local storage.
+// The event is numbered 1-11 because thats how many blocks there are in the
+// event planner. Second part is getting the var from above that is named timeBlock()
+// and adds a string of text blank to store the USER's entry.
+function localStorageEntry() {
+    localStorage.setItem("eventOne", timeBlock1.text());
+    localStorage.setItem("eventTwo", timeBlock2.text());
+    localStorage.setItem("eventThree", timeBlock3.text());
+    localStorage.setItem("eventFour", timeBlock4.text());
+    localStorage.setItem("eventFive", timeBlock5.text());
+    localStorage.setItem("eventSix", timeBlock6.text());
+    localStorage.setItem("eventSeven", timeBlock7.text());
+    localStorage.setItem("eventEight", timeBlock8.text());
+    localStorage.setItem("eventNine", timeBlock9.text());
+    localStorage.setItem("eventTen", timeBlock10.text());
+    localStorage.setItem("eventEleven", timeBlock11.text());
+};
+
+// Now that information is stored on local storage, it needs to be fetched
+// and displayed to the USER. Creating a new variable that it's sole purpose
+// is to GET that time block entry.
+var getTimeBlock1 = localStorage.getItem("eventOne");
+var getTimeBlock2 = localStorage.getItem("eventTwo");
+var getTimeBlock3 = localStorage.getItem("eventThree");
+var getTimeBlock4 = localStorage.getItem("eventFour");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Event Listeners to the SAVE button.
+
+// 0700 Time Block
+$("#saveOne").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 0800 Time Block
+$("#saveTwo").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 0900 Time Block
+$("#saveThree").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1000 Time Block
+$("#saveFour").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1100 Time Block
+$("#saveFive").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1200 Time Block
+$("#saveSix").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1300 Time Block
+$("#saveSeven").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1400 Time Block
+$("#saveEight").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1500 Time Block
+$("#saveNine").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1600 Time Block
+$("#saveTen").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
+
+  // 1700 Time Block
+$("#saveEleven").submit(function(event) {
+    event.preventDefault();
+    storeEntry();
+  });
